69c98a2c6650b381772b8a284e2520d2fdfb9bf3
Alisson Trindade Souza <alissonts2010@hotmail.com>
2025-05-31 21:36:02 -0300

    feat: add reactive login form

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, inject, OnInit } from "@angular/core";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
 
 @Component({
-    selector: 'login',
-    templateUrl: './login-reactive.component.html',
-    styleUrls: ['./login-reactive.component.css'],
-    standalone: false
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
+  standalone: false,
 })
 export class LoginReactiveComponent implements OnInit {
+  #fb = inject(NonNullableFormBuilder);
 
+  form = this.#fb.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: [
+      "",
+      [
+        Validators.required,
+        Validators.minLength(8),
+        createPasswordStrengthValidator(),
+      ],
+    ],
+  });
 
-  constructor() {
+  ngOnInit() {}
 
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
 
+    console.log(this.form);
   }
-
-  ngOnInit() {
-
-  }
-
 }
